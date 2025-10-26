4e8e60e28e4fd78f1af714d4da3c86188748af6e
Dounford-Felipe <92835771+Dounford-Felipe@users.noreply.github.com>
2025-10-26 08:11:00 -0300

    Added melee armors

---
+++ equipmentData.js
@@ -0,0 +1,202 @@
+const equipmentData = {
+	//Necklace
+    sapphire_necklace: {
+        name: "Sapphire Necklace",
+        accuracy: 2
+    },
+    emerald_necklace: {
+        name: "Emerald Necklace",
+        accuracy: 4
+    },
+    ruby_necklace: {
+        name: "Ruby Necklace",
+        accuracy: 6
+    },
+    diamond_necklace: {
+        name: "Diamond Necklace",
+        accuracy: 8
+    },
+
+	//Bronze
+    bronze_helmet: {
+        name: "Bronze Helmet",
+        defence: 1,
+        level: 1
+    },
+    bronze_body: {
+        name: "Bronze Body",
+        defence: 1,
+        level: 1
+    },
+    bronze_legs: {
+        name: "Bronze Legs",
+        defence: 1,
+        level: 1
+    },
+    bronze_gloves: {
+        name: "Bronze Gloves",
+        defence: 1,
+        level: 1
+    },
+    bronze_boots: {
+        name: "Bronze Boots",
+        defence: 1,
+        level: 1
+    },
+
+    //Iron
+    iron_helmet: {
+        name: "Iron Helmet",
+        defence: 2,
+        level: 10
+    },
+    iron_body: {
+        name: "Iron Body",
+        defence: 2,
+        level: 10
+    },
+    iron_legs: {
+        name: "Iron Legs",
+        defence: 2,
+        level: 10
+    },
+    iron_gloves: {
+        name: "Iron Gloves",
+        defence: 2,
+        level: 10
+    },
+    iron_boots: {
+        name: "Iron Boots",
+        defence: 2,
+        level: 10
+    },
+
+    //Silver
+    silver_helmet: {
+        name: "Silver Helmet",
+        defence: 3,
+        level: 20
+    },
+    silver_body: {
+        name: "Silver Body",
+        defence: 3,
+        level: 20
+    },
+    silver_legs: {
+        name: "Silver Legs",
+        defence: 3,
+        level: 20
+    },
+    silver_gloves: {
+        name: "Silver Gloves",
+        defence: 3,
+        level: 20
+    },
+    silver_boots: {
+        name: "Silver Boots",
+        defence: 3,
+        level: 20
+    },
+
+    //Gold
+    gold_helmet: {
+        name: "Gold Helmet",
+        defence: 4,
+        level: 30
+    },
+    gold_body: {
+        name: "Gold Body",
+        defence: 4,
+        level: 30
+    },
+    gold_legs: {
+        name: "Gold Legs",
+        defence: 4,
+        level: 30
+    },
+    gold_gloves: {
+        name: "Gold Gloves",
+        defence: 4,
+        level: 30
+    },
+    gold_boots: {
+        name: "Gold Boots",
+        defence: 4,
+        level: 30
+    },
+
+    //Promethium
+    promethium_helmet: {
+        name: "Promethium Helmet",
+        defence: 6,
+        level: 50
+    },
+    promethium_body: {
+        name: "Promethium Body",
+        defence: 6,
+        level: 50
+    },
+    promethium_legs: {
+        name: "Promethium Legs",
+        defence: 6,
+        level: 50
+    },
+    promethium_gloves: {
+        name: "Promethium Gloves",
+        defence: 6,
+        level: 50
+    },
+    promethium_boots: {
+        name: "Promethium Boots",
+        defence: 6,
+        level: 50
+    },
+
+    //Titanium
+    titanium_helmet: {
+        name: "Titanium Helmet",
+        defence: 7,
+        level: 65
+    },
+    titanium_body: {
+        name: "Titanium Body",
+        defence: 7,
+        level: 65
+    },
+    titanium_legs: {
+        name: "Titanium Legs",
+        defence: 7,
+        level: 65
+    },
+    titanium_gloves: {
+        name: "Titanium Gloves",
+        defence: 7,
+        level: 65
+    },
+    titanium_boots: {
+        name: "Titanium Boots",
+        defence: 7,
+        level: 65
+    },
+
+    //Hats
+    straw_hat: {
+        name: "Straw Hat",
+        bonus: "+10 farming levels on harvesting"
+    },
+    
+    //Weapons
+    titanium_mace: {
+        name: "Titanium Mace",
+        damage: 10,
+        accuracy: 8,
+        level: 70
+    },
+
+    //Sigils
+    tree_sigil: {
+        name: "Tree Sigil"
+    }
+
+    //Pets
+};
